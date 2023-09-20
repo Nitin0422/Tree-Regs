6cd7d4d9c9418fa5f05f99151a39a141ad0defa1
Nitin Tandukar <nitintandukar@Nitins-MacBook-Air.local>
2023-09-20 08:47:16 +0545

    Fixed a typo in foreign key

--- RS/models.py
+++ RS/models.py
@@ -12,5 +12,5 @@ class ETRS(models.Model):
     tree_name = models.CharField(max_length=200)
 
 class CTRS(models.Model):
-    rree_id = models.ForeignKey(LandDetails, on_delete=models.CASCADE)
+    tree_id = models.ForeignKey(LandDetails, on_delete=models.CASCADE)
     tree_name = models.CharField(max_length=200)
